bd74a488f4c819f53036601457e58b08c526839e
beatriz <44908994+bvechiato@users.noreply.github.com>
2022-03-28 20:47:38 +0100

    precarious error handling
    using webdriverwait
    
    + goes directly to the login-in page instead of through learn then takes you to myed

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-driver.get("https://www.learn.ed.ac.uk/")
+driver.get("https://www.ease.ed.ac.uk/cosign.cgi?cosign-eucsCosign-www.myed.ed.ac.uk&https://www.myed.ed.ac.uk/uPortal/Login?refUrl=%2Fmyed-progressive%2F")
 
 data = read_contents()
 username = data[0]
 password = data[1]
 
-# click login button
-driver.find_element(By.XPATH, "//*[contains(concat( \" \", @class, \" \" ), concat( \" \", \"easelogin-bt\", \" \" ))]").click()
-
 # enter username and proceed
-driver.find_element(By.XPATH, "//*[(@id = \"login\")]").send_keys(username)
-driver.find_element(By.XPATH, "//*[(@id = \"submit\")]").click()
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"login\")]"))).send_keys(username)
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"submit\")]"))).click()
 
 # enter password and log in
-driver.find_element(By.XPATH, "//*[(@id = \"password\")]").send_keys(password)
-driver.find_element(By.XPATH, "//*[(@id = \"submit\")]").click()
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"password\")]"))).send_keys(password)
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"submit\")]"))).click()
