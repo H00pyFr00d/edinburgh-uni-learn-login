87fff99f910e24ae4dca74b86efdfd24840cd5fd
Christopher Dalziel <kiff2002@gmail.com>
2022-04-03 14:36:31 +0100

    Keyring Support + FileNotFound handling
    Changed from storing passwords in a plaintext file to storing them using the python keyring
    
    Also will create the userinfo.txt file if it doesn't exist (storing the username only now)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from getUserInfo import read_contents
+import os
 
+import keyring
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -14,14 +15,33 @@ chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.get("https://www.ease.ed.ac.uk/cosign.cgi?cosign-eucsCosign-www.myed.ed.ac.uk&https://www.myed.ed.ac.uk/uPortal/Login?refUrl=%2Fmyed-progressive%2F")
 
-data = read_contents()
-username = data[0]
-password = data[1]
+try:
+    with open(os.getcwd() + "\\userinfo.txt", "r") as f:
+        username = f.read().splitlines()[0]
+        f.close()
+except FileNotFoundError:
+    with open(os.getcwd() + "\\userinfo.txt", "w") as f:
+        while True:
+            username = input("\nEnter your UUN (s1234567): ")
+            if (input("Is " + username + " correct? (y/n): ").lower().strip() == "y") & (len(username) == 8) & (username[0] == "s"):
+                f.write(username)
+                break
+        f.close()
+
+if keyring.get_password("MyEd", username) is None:
+    print("\nPassword not found for " + username)
+    keyring.set_password("MyEd", username, input("Please enter your MyEd password: "))
 
 # enter username and proceed
 WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"login\")]"))).send_keys(username)
 WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"submit\")]"))).click()
 
 # enter password and log in
-WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"password\")]"))).send_keys(password)
+WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"password\")]"))).send_keys(
+    keyring.get_password("MyEd", username))
 WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, "//*[(@id = \"submit\")]"))).click()
+
+# if we ended up at this page again we've got a wrong user or password
+if driver.current_url.find("https://www.ease.ed.ac.uk/cosign.cgi"):
+    print("Username or password is incorrect, please check the userinfo.txt and run the program again.")
+    keyring.delete_password("MyEd", username)
